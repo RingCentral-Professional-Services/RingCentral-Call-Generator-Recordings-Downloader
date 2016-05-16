ba0a6010e372498482611d7d82a57f9e95d31372
Colin Liu <colin198702@gmail.com>
2016-05-16 23:15:20 +0800

    Eliminate loop

--- modules/file_struct_s3.php
+++ modules/file_struct_s3.php
@@ -8,11 +8,14 @@ if($callLog->direction == "Inbound") {
     $number = $callLog->from->phoneNumber;
 }
 
-$extension = getExtension($number, $global_phoneNumbers, $global_accountExtensions);
-if(!is_null($extension)){
-    array_push($filePaths, $extension->name, substr($callLog->startTime, 0, 10), 
-        substr($callLog->startTime, 11, 8)."_".$callLog->recording->id.".".$recording['ext']);
-}else {
-    array_push($filePaths, $number, substr($callLog->startTime, 0, 10), 
-        substr($callLog->startTime, 11, 8)."_".$callLog->recording->id.".".$recording['ext']);
-}
+// $extension = getExtension($number, $global_phoneNumbers, $global_accountExtensions);
+// if(!is_null($extension)){
+//     array_push($filePaths, $extension->name, substr($callLog->startTime, 0, 10), 
+//         substr($callLog->startTime, 11, 8)."_".$callLog->recording->id.".".$recording['ext']);
+// }else {
+//     array_push($filePaths, $number, substr($callLog->startTime, 0, 10), 
+//         substr($callLog->startTime, 11, 8)."_".$callLog->recording->id.".".$recording['ext']);
+// }
+
+array_push($filePaths, $number, substr($callLog->startTime, 0, 10), 
+    substr($callLog->startTime, 11, 8)."_".$callLog->recording->id.".".$recording['ext']);
