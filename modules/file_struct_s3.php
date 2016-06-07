0602ef0a19badf178bf4dcbc2b017a0abe24c803
Colin Liu <colin198702@gmail.com>
2016-06-07 22:13:07 +0800

    Fix bugs

--- modules/file_struct_s3.php
+++ modules/file_struct_s3.php
@@ -1,26 +1,48 @@
 <?php
 
-$number = null;
+$number = 'unknown';
+$indicator = null;
+$otherNumber = 'unknown';
 
 if($callLog->direction == "Inbound") {
+    $indicator = "from";
     if(property_exists($callLog->to, 'phoneNumber')){
         $number = $callLog->to->phoneNumber;
     }else{
-        $number = $callLog->to->extensionNumber;
+        if(property_exists($callLog->to, 'extensionNumber')){
+            $number = $callLog->to->extensionNumber;
+        }
+    }
+    if(property_exists($callLog->from, 'phoneNumber')){
+        $otherNumber = $callLog->from->phoneNumber;
+    }else{
+        if(property_exists($callLog->from, 'extensionNumber')){
+            $otherNumber = $callLog->from->extensionNumber;
+        }
     }
 }else{
+    $indicator = "to";
     if(property_exists($callLog->from, 'phoneNumber')){
         $number = $callLog->from->phoneNumber;
     }else{
-        $number = $callLog->from->extensionNumber;
+        if(property_exists($callLog->from, 'extensionNumber')){
+            $number = $callLog->from->extensionNumber;
+        }
+    }
+    if(property_exists($callLog->to, 'phoneNumber')){
+        $otherNumber = $callLog->to->phoneNumber;
+    }else{
+        if(property_exists($callLog->to, 'extensionNumber')){
+            $otherNumber = $callLog->to->extensionNumber;
+        }
     }
 }
 
 $extension = getExtension($number, $phoneNumbers, $accountExtensions);
 if(!is_null($extension)){
     $filePath = ($extension->name).'/'.substr($callLog->startTime, 0, 10).'/'.
-        substr($callLog->startTime, 11, 8)."_".$callLog->recording->id;
+        $indicator."_".$otherNumber."_".substr($callLog->startTime, 11, 8)."_".$callLog->recording->id;
 }else {
     $filePath = $number.'/'.substr($callLog->startTime, 0, 10).'/'.
-        substr($callLog->startTime, 11, 8)."_".$callLog->recording->id;
+        $indicator."_".$otherNumber."_".substr($callLog->startTime, 11, 8)."_".$callLog->recording->id;
 }
